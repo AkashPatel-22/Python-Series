f9a75b38af1e8e5d712abadcf65ede0cedf50eff
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-04 15:22:03 +0530

    day 1 push

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello world")
+#print() is used to print,or display in terminal window(console)
+#output will be 'hello world'
+#run command in console - python filename tab enter.
+
+# we can also print numerical values 
+print(3.14)# 3.14
+print(2+3)#5
+
+print("hello world", "from PRIME") # multiple values can be printed on same line
+print("hello world from PRIME")
---
+++ input.py
@@ -0,0 +1,15 @@
+#input()
+#it is use to take input from user while the programm is running
+
+name = input("enter name: ")
+print(name)
+print(type(name))
+
+#type conversion () to convert the type of input
+
+a = int(input("enter num1:" ))
+b = int(input("enter num2:" ))
+
+c = a+b 
+print(c)
+print(a+b)
