6240acdee4e9ad8711fea76c0745999b19370ac7
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-05 09:49:51 +0530

    Python character set & variables

---
+++ Day-2/charachterset.py
@@ -0,0 +1,9 @@
+#character set is a collection of characters, symbols, numbers
+#multiline comments like /* ... */
+#/*eng letters: 'a' - 'z' & 'A' to 'Z'
+# digits: 0 to 9
+# special symbols: +,-,*,/,%  
+# blank space = (  )
+# tab space = (\t)
+# new line = (\n)
+# */
---
+++ Day-2/variables.py
@@ -0,0 +1,16 @@
+#Variable /*
+# A variable is like a container that stores data or values in our prgrm
+# and can store dat of many  type ( nujmbers, text etc) 
+# 
+# some examples... */
+
+name = "akash"
+age = 30
+PI = 3.14
+word = "AI journey"
+
+print(" the value PI",PI)
+
+
+# variable start with (a-z, A-Z) or underscore_ only
+#conatain letter, numnbers,or underscore after the first characters
