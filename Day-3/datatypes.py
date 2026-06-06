77ae9e346c4d7b4bbbdc94b353cead47fa87d51f
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-06 08:38:34 +0530

    DataTypes & TypeConversion

---
+++ Day-3/datatypes.py
@@ -0,0 +1,27 @@
+# data type /* .
+# a data type defines the kind of value a variables hold.
+# /* 
+# int = integer values(whole number)
+# float =  decimal values
+# str = str, chacters,words or sentences
+# bool = true/false 
+# none = no value / null
+
+# some examples....
+
+a = 10
+print(type(a))
+
+PI = 3.14
+print(type(PI))
+
+name = "akash patel"
+print(type(name))
+
+isTeacher = True
+print(type(isTeacher))
+
+empty_var = None
+print(type(empty_var))
+
+
---
+++ Day-3/typeconversion.py
@@ -0,0 +1,16 @@
+#type conversion - done automatically by pyhton
+# a = 5
+# b = 3.0
+# print(a+b) # python converts ans in float by default
+# output = 8.0
+
+#type casting - done by the programmer
+a = 10
+x = float(a) #10.0
+print(x)
+
+y = str(a) # 10
+print(y)
+
+z = bool(a) # true
+print(z)
