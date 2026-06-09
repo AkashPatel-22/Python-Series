6345f34a8dce6b5c3a926f2732dd2e3946febc1f
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-09 11:04:45 +0530

    Ternary Statement

---
+++ Day-6/ternary_statement.py
@@ -0,0 +1,12 @@
+# Ternary Statement :- one line conditional statement
+
+# -- Ternary statement are just anothor compact way to writing our 'if-else'
+#  statement in one line.
+
+# -- One line decided between 2 values based on condition.
+
+# example:-
+
+age = 18
+status = "adult" if age >= 18 else "not adult"
+print(status)
