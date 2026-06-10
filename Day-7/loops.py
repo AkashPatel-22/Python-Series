ca77c579b83c2301ecf8af326070bc5d202e23a7
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-10 09:13:41 +0530

    About While Loops

---
+++ Day-7/loops.py
@@ -0,0 +1,33 @@
+#  Loops:
+# -Execute the block of code multiple time.
+
+# /*           Two main types of loop in python.
+#              1. while loop  
+#              2.for loop 
+# 
+
+# While Loop :- "repeat while a condition is true".
+
+
+# syntax:
+# while condition:
+    #code block
+    
+    
+    
+# some examples :- */
+
+# example 1 :- print 1 to 5 using while loop
+
+i = 1
+while i <= 5:
+    print(i)
+    i+=1
+    
+# example 2 :- print 5 to 1    
+print("\n")
+
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
