edafcadbb239daaf7045f88f61c148c56b14002c
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-12 15:05:18 +0530

    Loop Control Statement

---
+++ Day-9/loopcontrol_statements.py
@@ -0,0 +1,23 @@
+# Loop Control Statement..
+
+# Break Keyword :- it stops the loop immediately
+
+# example :- break for multiple of 6
+
+i = 1
+
+while i <= 10:
+    if i % 6 == 0: #checking multiple of 6
+        break # jse he multiple milega break ho jayega
+    print(i)
+    i += 1
+    
+# continue keyword :-  skip the current interation ans move to the next one
+
+i = 0
+while i < 10:
+    i += 1
+    if i % 3 == 0:
+        continue;
+    print(i)
+     
