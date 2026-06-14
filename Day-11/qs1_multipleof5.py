c9bd8b109c1f186fbdc2da4577a0818b8a3929b4
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-14 07:17:56 +0530

    Practice-Qs

---
+++ Day-11/qs1_multipleof5.py
@@ -0,0 +1,8 @@
+# for given number n, print if it is multiple of 5 of not.
+
+n = int(input("enter number: "))
+
+if n % 5 == 0 :
+    print("it is multiple of 5 ")
+else:
+    print("not a multiple of 5")    
