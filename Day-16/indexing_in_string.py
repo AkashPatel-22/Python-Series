508d5a4f6c1d2c66c62d401d98d182f1a81a9dc9
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-19 09:30:02 +0530

    Indexing in String

---
+++ Day-16/indexing_in_string.py
@@ -0,0 +1,15 @@
+# Indexing in String
+# -- index in sequence represents the position value of individual elements
+# i.e. characters in case of string . so indexing lets us access individual
+# characters in string.
+
+
+#  python follows Zero-Based indexing i.e. first character is at index -'0'.
+
+# example
+
+s = "Python"
+
+print(s[0]) # 'P' 
+print(s[3]) # 'h' 
+print(s[-1]) # 'n' --> (negative index: last character) 
