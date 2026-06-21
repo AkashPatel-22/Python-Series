eb98a2146114b3388a18d7311e6451b713a7b4cb
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-21 19:05:16 +0530

    list indexing

---
+++ Day-17/indexing_lists.py
@@ -0,0 +1,13 @@
+# List indexing..
+# -index in a list is the position value of an item, index starts from 0.
+
+# Access Elements.
+my_list = ["apple","banana","cherry"]
+print(my_list[0]) #apple
+print(my_list[1]) #banana
+print(my_list[-1]) # last elements 
+
+# Modifying Elements.
+my_list = [1,2,3,4,5] 
+my_list[0] = 10 # inserting elements or modifying.
+print(my_list)
