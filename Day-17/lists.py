3692924b671ac597f46a28c0e84465c7fcaedb56
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-21 18:58:32 +0530

    List and their characteristicks

---
+++ Day-17/lists.py
@@ -0,0 +1,21 @@
+# List..
+# -a list is a collection of items in python.
+
+# items in a list are:
+# *ordered -> defined ordered and can be accessed by index. 
+# *changebale(mutable) -> items can be changed after the  list is created. 
+# *allows duplicates -> same value can be appear more than once.
+# *hetrogeneous -> contain different data types.
+
+# - list are written using square brackets [].
+
+# example..
+my_list = [1,2,3,4,5] 
+print(my_list)
+print(type(my_list))
+
+print("\n")
+
+my_list1 = [1,'hello',3.14,True,10] # heterogenous list
+print(my_list1)
+print(type(my_list))
