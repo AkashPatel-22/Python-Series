b82a3a418d4482314a034ae81a31e14fc289e7f3
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-22 17:55:40 +0530

    List Methods

---
+++ Day-18/method_list.py
@@ -0,0 +1,22 @@
+# List Methods.
+
+# len() - length of the string.
+# append(elements)- adds an elements to the end.
+# insert(idx, elements) - add at specific position.
+# sort() - sorts in ascending/alphabetical order.
+# reverse() - flips the order of elements.
+
+nums = [5, 9, 2]
+# print(len( nums)) #3
+
+nums.append(10)
+print(nums)
+
+nums.insert(1,55)
+print(nums)
+
+nums.sort()
+print(nums)
+
+nums.reverse()
+print(nums)
