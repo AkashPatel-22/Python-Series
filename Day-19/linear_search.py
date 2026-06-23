31904ed748d16cce8a82317d732416b1e79728ba
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-23 08:45:43 +0530

    loop on list

---
+++ Day-19/linear_search.py
@@ -0,0 +1,10 @@
+#  Linear Search - checks elements one by one to find target (x).
+ 
+number = [10, 20, 30, 40, 18, 50]
+x = 18
+idx = 0
+for num in number:
+    if num == x:
+        print(f"{x} found at index = {idx}")
+    idx += 1    
+    
---
+++ Day-19/list_loop.py
@@ -0,0 +1,10 @@
+# Loops On Lists.
+# -- we use the classical 'for' loop to travers the entire list elements wise.
+
+# the most common way is using a loop in list is to print elements
+
+# example :-
+number = [10, 20, 30, 40, 50]
+for num in number:
+    print(num)
+    
