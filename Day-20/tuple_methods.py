71c8f050f98aa257d1c38c1b90ebee4a3489fb1e
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-24 09:58:00 +0530

    Tuple Methods

---
+++ Day-20/tuple_methods.py
@@ -0,0 +1,7 @@
+# Tuple Methods.
+# *index(val) - returns index of first occurence of any value.
+# *count(val) - returns total count of occurence for any value.
+
+t = (1,2,2,2,5)
+print(t.index(2)) # 1
+print(t.count(2)) # 3
