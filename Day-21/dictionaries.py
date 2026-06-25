076282039d1cd2e3c69a0eb5f64b89b56a06dfb8
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-25 10:05:44 +0530

    Dictionary

---
+++ Day-21/dictionaries.py
@@ -0,0 +1,19 @@
+# Dictionary :
+#  - ordered, mutubale collection of key value pairs.
+
+my_dict = {
+    "name" : "shradha",
+    "age" : 30,
+    "city" : "delhi"
+}
+
+# Dictionary characteristic.
+
+# - dictionary key must be unique
+# - dictionary key must be immutable (str,no,tuples)
+# - value ca be any thing ( list, other dictionaries)
+
+# Accessing Value (using key & [])
+student = { "name": "bob", "age":20}
+print(student["name"]) # bob
+
---
+++ Day-21/loops_on_dictionary.py
@@ -0,0 +1,15 @@
+# 
+
+
+
+# Loops on Dictionary.
+
+
+my_dict = {
+    "name" : "shradha",
+    "age" : 30,
+    "city" : "delhi"
+}
+
+for key,value in my_dict.items():
+    print(key, value)
