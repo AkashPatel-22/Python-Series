6cd5ab05e40559a25e195a1a4590defabf379069
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-26 12:11:20 +0530

    Sets in python

---
+++ Day-22/sets.py
@@ -0,0 +1,16 @@
+# Sets
+# *- a set is an unordered collection of unique elements.
+
+set = {1,2,2,2,3}
+print(set)  # {1,2,3}
+print(type(set))
+print(len(set)) # 3
+
+# empty_set = set()
+
+# characteristics.
+# - sets can only have unique elements.
+# - they are unordered- no indexing or slicing.
+# - they are mutable- we can add or remove elements.
+# - sets elements must be immutable(like str,no,tuples)
+
