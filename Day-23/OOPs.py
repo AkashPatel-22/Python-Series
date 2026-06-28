acc038d374836b0fed70feb28095d754c5c0fc0d
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-06-28 10:00:15 +0530

    Object Oriented Programming

---
+++ Day-23/OOPs.py
@@ -0,0 +1,18 @@
+# what is Object Oriented Programming (OOP)
+
+# -- Object-Oriented Programming(OOP) in python is aprogramming paradigm centered
+# around organizing code into objects. bundles of data (attributes) and behaviour
+# (methods).
+
+# -- it helps us structure programs in a way thta is modular, reusable, and easier
+#  to maintain
+
+
+# NOTE - it's not compulsory to always use OOP concepts but they definitely help
+# in a lot of cases.
+
+# OOP models software as a collection of interacting objects, similar to how we think
+# about real-world entities.Each objects represents something meaningful in our program
+# such as a user, a car, a bank account, or an enemy in a video game.
+
+# in python , OOP is based around classes, which are blueprints for creating objects.
