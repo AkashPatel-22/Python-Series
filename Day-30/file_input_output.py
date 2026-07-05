d2f352804cb7751af925863b7ee0a0af1efe6500
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-05 08:41:54 +0530

    File I/O

---
+++ Day-30/file_input_output.py
@@ -0,0 +1,14 @@
+# File I/O.
+# file input /output means- reading data from files and writing data to files using python
+# python privides built in function to work with files.
+
+# Opening a File.
+# - we use the open() function.
+
+# syntax.
+# file = open("filename","mode")
+
+# example.
+f = open("data.txt","r")
+print("complete")
+
