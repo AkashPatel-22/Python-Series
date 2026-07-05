bee075272958b126c3376c6b8021425ae7a7ac2e
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-05 08:44:53 +0530

    Common Modes

---
+++ Day-30/common_modes.py
@@ -0,0 +1,25 @@
+''' 
+Common Modes
+
+1. r - Read
+   Opens file for reading (default). Error if file doesn't exist.
+
+2. w - Write
+   Creates new file or overwrites existing file.
+
+3. a - Append
+   Adds content at the end of the file.
+
+4. r+ - Read + Write
+   File must exist.
+
+5. w+ - Write + Read
+   Overwrites existing file.
+
+6. a+ - Append + Read
+   Reads + adds to end of file.
+
+7. b - Binary Mode
+   For images, videos, etc. (use with other modes like rb).
+
+'''
