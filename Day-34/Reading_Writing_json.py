c0299aba8b6f1efb6c174a922a1c821b567ff538
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-09 08:57:35 +0530

    Reading Writing in JSON

--- Day-34/Reading_Writing_json.py
+++ Day-34/Reading_Writing_json.py
@@ -1,17 +1,17 @@
 # Reading Json from a file (json.load)
 
-# with open("data.json","r") as f:
-#     data = json.load(f)
+with open("data.json","r") as f:
+    data = json.load(f)
     
-#     print(data)
+    print(data)
 
 
 # writing Json to a file (json.dumps)
 
-# data = {"name": "akash","age":23, "marks": [85,90,92]}
+data = {"name": "akash","age":23, "marks": [85,90,92]}
 
-# with open("data.json","w") as f:
-#     json.dump(data,f,indent=4)
+with open("data.json","w") as f:
+    json.dump(data,f,indent=4)
     
     
 #  indent = 4 makes the JSON looks neat and readable
