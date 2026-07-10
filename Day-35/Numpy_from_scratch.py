567fb273f3047f3a67e0c8c76280032a4402a3ed
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-10 09:52:40 +0530

    Creating Numpy Arrays - from scratch

--- Day-35/Numpy_from_scratch.py
+++ Day-35/Numpy_from_scratch.py
@@ -16,5 +16,5 @@ arr3 = np.full((2,3),5) #2*3 arrays of 5s
 print(arr3, arr3.shape)
 print("\n")
 
-# arr1 = np.zeros(3*4)
-# print(arr1, arr1.shape)
+arr4 = np.eye(3) # Identity matrix of 3*3
+print(arr4, arr4.shape)
