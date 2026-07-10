35f906bfed4a2912cad730b366970d253ac2aabb
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-10 09:22:08 +0530

    All about Numpy (Numerical Python)

---
+++ Day-35/Numpy.py
@@ -0,0 +1,30 @@
+# What is Numpy?
+# - Numpy (Numerical Python) is an open source python library used for numerical computions.
+#  it provides tools for working efficiently with arrays,matrices, and mathematical functions.
+
+
+# Numpy introdues new data structures called the ndarray (n-dimensional array),
+# which is much faster and more memory-efficient than python's built in list
+#  for numerical operations.
+
+# Important Features.
+# 1. Multidimensional Arrays:
+# - Efficient storage and manipulation of large datasets (e.g.1D,2D,3D arrays).
+
+# 2. Mathematical Functions:
+# - a wide range of mathematical operations- linear algebra, fourier transform etc.
+
+# 3. Vectorization:
+# - Operations on entire arrays without explicit loops,making code cleaner and faster.
+
+# 4. Interoperablility:
+# - works well with other libraries like pandas,matplotlib,tensorflow etc.
+
+# Usage
+
+import numpy as np
+
+# create & output an array
+
+arr = np.array([1,2,3,4,5])
+print(arr)
