7cbfcc7d30f90fe11f9df8fc2831b42a2328761a
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-12 08:49:24 +0530

    USAGE of multidimensional array.

---
+++ Day-37/Usage_MDA.py
@@ -0,0 +1,12 @@
+# USAGE of multidimensional array.
+
+# /****
+#  we'll look into a practical example of dealing with real-world immages data.
+# if we have a ML/DL model working with images, then we can feed this data as 2D or 3D arrays.
+# 
+# 1. Grayscale Image as 2D arrays
+# -a grayscale image has height *width pixels. Each pixel has a single intensity value (0-255 for 8-bit images)
+# 
+# 2. Color Images.
+# A color image(RGB) has height*width*channels. the 3 channels are -
+# RGB (Red,Green,Blue) & each channel is a 2D array of pixel intensities (0-255)***/
