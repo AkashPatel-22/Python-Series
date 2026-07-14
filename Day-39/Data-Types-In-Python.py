5f129d35a0a57f0de555c5be552085b05f603d71
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-14 08:21:28 +0530

    Data Types in Python

---
+++ Day-39/Data-Types-In-Python.py
@@ -0,0 +1,18 @@
+# Data Types.
+
+#  - We have already discussed how every Numpy array has a single data type.
+# (homogeneous arrays) and it is stored in the .dtype attribute.
+
+# - the most common data types in NumPy
+
+# 1. Integer literals : int32,int64
+
+# 2. floating literals : float32,float64
+
+# 3. Boolean : bool
+
+# 4. Complex numbers : complex64, complex128
+
+# 5. String : s(byte-str)& u (unicode-str)
+
+# 6. Object : generic python objects - object
