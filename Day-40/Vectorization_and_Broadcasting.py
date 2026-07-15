5f7deb451de2d912315ef3cc262577978c988863
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-15 09:22:37 +0530

    Vectorization & Broadcasting

--- Day-40/Vectorization_and_Broadcasting.py
+++ Day-40/Vectorization_and_Broadcasting.py
@@ -15,3 +15,29 @@ print(sq_arr)
 
 arr2 = np.array([6,7,8,9,10])
 print(arr + arr2)   # sum of two arrays
+
+
+# Broadcasting
+# - allows numpy to automatically expand arrays of different shapes so that arithmetic operations can be performed.
+# - it is basically scaling arryas without using extra money
+# -- no neend to manually re shape arrays.
+# -- usefull for combining arrays of different dimensions.
+
+# Broadcasting Rule.
+# - Broadcasting can only take place when the arrays are of compatible shape.so numpy compares shapes of arrays
+#  FROM RIGHT TO LEFT . for the array to be compatible, all dimensions must either be:
+# -- equal or
+# -- 1, or
+# -- missing (smaller arrays can be "stretched")
+
+
+# Broadcasting with a Scaler.
+import numpy as np
+
+arr_mul10 = arr * 10  # multiply by 10 to all numbers
+print(arr_mul10)
+
+#  Broadcasting with a vector
+arr1d = np.array([1,2,3])
+arr2d = np.array([[1,2,3],[4,5,6]])
+print(arr1d+arr2d)
