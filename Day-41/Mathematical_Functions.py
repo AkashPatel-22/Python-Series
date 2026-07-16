6891d85d2e5ec2dcfdd8b5dfe0651835e84d8b14
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-16 11:22:43 +0530

    Mathematical Functions in Python

---
+++ Day-41/Mathematical_Functions.py
@@ -0,0 +1,15 @@
+# Mathematical Functions in Python
+# - Numpy is massive and provides a wide range of built-in mathematical functions that are highly optimized
+# and can operate on element-wise on arrays. let's have a look at some of them.
+
+
+# aggregate functions
+# - sum() : returns the sum of array elements over a given axis.
+# - mean() : returns the average of array elements over a given axis.
+# - std() : returns the standard deviation of array elements over a given axis.
+# - var() : returns the variance of array elements over a given axis.   
+# - min() : returns the minimum value of array elements over a given axis.
+# - max() : returns the maximum value of array elements over a given axis.
+# - median() : returns the median of array elements over a given axis.
+# - prod() : returns the product of array elements over a given axis.
+# - 
