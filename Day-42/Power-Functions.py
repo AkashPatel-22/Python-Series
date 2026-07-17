92ccf5162212194fb6c06cb509e8ab3b20cbf088
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-17 10:02:14 +0530

    Power Functions

---
+++ Day-42/Power-Functions.py
@@ -0,0 +1,7 @@
+# Power Functions in Python
+# - Numpy provides a wide range of built-in power functions that are highly optimized and can operate on element-wise on arrays. 
+# let's have a look at some of them.
+
+# 1.square() : returns the square of array elements.
+# 2.sqrt() : returns the square root of array elements.
+# 3.cbrt() : returns the cube root of array elements.
