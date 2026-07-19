f391d2bf459416d73e5d039aab0dbd95dde6d8c4
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-19 08:34:48 +0530

    what is pandas

---
+++ Day-44/Pandas.py
@@ -0,0 +1,13 @@
+# PANDAS.
+# - pandas is one of the most widely used python libraries for data analysis and manipulations.
+#  it is open-source amd built on the top of numpy. it adds high level data structures and tools that make
+#  it easier to work with tabukar , labeled, or hetrogeneous datasets.
+
+#  Data Structures.
+# -- pandas introduces two important data structures : series & DataFrame
+
+
+# usage
+import pandas as pd
+df = pd.DataFrame({"Name" :["alice","bob"],"CGPA":[9.5,8.7]})
+print(df)
