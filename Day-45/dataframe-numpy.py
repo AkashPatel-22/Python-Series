cea36306b34ec2ef6e653bf62479c33aeb6e80ea
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-20 09:18:45 +0530

    Creating DataFrame using Numpy array.

--- Day-45/dataframe-numpy.py
+++ Day-45/dataframe-numpy.py
@@ -1,5 +1,6 @@
 # Creating DataFrame using Numpy array.
 import numpy as np
+import pandas as pd
 np_arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
 df = pd.DataFrame(np_arr, columns = ["col1","col2","col3"])
 print(df)
