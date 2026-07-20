4f7c1de6d3ab1e36140f0713ca4c63e3d017234e
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-20 09:13:49 +0530

    DataFrame in pandas

--- Day-45/dataframe-in-pandas.py
+++ Day-45/dataframe-in-pandas.py
@@ -8,3 +8,20 @@
 # -- Columns labels
 
 # Each columns in DataFrame is a series
+
+# usage.
+
+import pandas as pd
+# creating DataFrame in pandas -  using dictionary.
+info = {
+    "Name" : ["Adam","Eve","Bob"],
+    "Marks" : [78,99,85],
+    "Grade" : ['B','O','A']
+}
+
+df = pd.DataFrame(info)
+print(df)
+print(type(df))
+
+print(df.index)   # row lables
+print(df.columns)   # columns lables
