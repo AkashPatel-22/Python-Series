686282d0d357d70b277b3ee42a0cce7eac8efc34
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-20 09:12:29 +0530

    DataFrame

---
+++ Day-45/dataframe-in-pandas.py
@@ -0,0 +1,10 @@
+#  DataFrame.
+# - a DataFrame is a two dimensional,tabular data structures (like a spreadsheet or SQL table).
+
+#  It consits of:
+# -- Rows
+# -- Columns
+# -- Index (row lables)
+# -- Columns labels
+
+# Each columns in DataFrame is a series
