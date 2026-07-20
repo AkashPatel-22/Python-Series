618074f49fbfc07ab045cf83e54d0770f73c1fcb
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-20 09:19:29 +0530

    Creating DataFrame using Lists

--- Day-45/dataframe-numpy.py
+++ Day-45/dataframe-numpy.py
@@ -4,3 +4,8 @@ import pandas as pd
 np_arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
 df = pd.DataFrame(np_arr, columns = ["col1","col2","col3"])
 print(df)
+
+# Creating DataFrame using Lists.
+l = [["adam",96],["eve",75],["bob",85],["charlie",92]]
+df = pd.DataFrame(l,columns = ["Name","Marks"])
+print(df)
