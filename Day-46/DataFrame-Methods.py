c3143c2737db5abb5defed3710f934dbdf78679b
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-21 10:52:43 +0530

    DataFrame Methods in Pandas.

---
+++ Day-46/DataFrame-Methods.py
@@ -0,0 +1,14 @@
+# DataFrame Methods in Pandas.
+
+# - we have a lot of useful methods with dataframe.
+
+# Data Viewing and Inspection:
+# - these help us take a quick look at our datasets.
+
+# 1. df.head(n) - shows first n rows (default=5)
+# 2. df.tail(n) - shows last n rows (default=5)
+
+# 3. df.sample(n) - shows random n rows (deafult=1)
+# 4. df.info() - Displays column names, datatypes, memory usage.
+
+# 5. df.describe(n) - shows descriptive statistics for numeric columns
