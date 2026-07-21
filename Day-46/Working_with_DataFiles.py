b7445fe61c8eb04fed215c7180b631dba9c08644
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-21 10:40:47 +0530

    Pandas with json Files.

--- Day-46/Working_with_DataFiles.py
+++ Day-46/Working_with_DataFiles.py
@@ -4,3 +4,11 @@ import pandas as pd
 
 data = pd.read_csv("Global_air_quality.csv")
 print(data)
+
+
+#  Pandas with json Files.
+
+import pandas as pd
+
+j_data = pd.read_csv("data.json")
+print(j_data)
