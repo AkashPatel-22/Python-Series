5dce89f29191e6927b6ebd2ca2cccdde661a3862
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-22 10:21:58 +0530

    Selecting Rows

--- Day-47/Indexing&DataSelection.py
+++ Day-47/Indexing&DataSelection.py
@@ -17,3 +17,24 @@ df = pd.DataFrame(data)
 
 df['City']  # returns the 'City' column as a Series
 df[['Name', 'Age']]  # returns a DataFrame with 'Name' and 'Age' columns
+
+
+# Selecting Rows:
+
+# - we can select rows using the index or by using conditions.
+# - By index labels .loc[]  - use row labels to select rows.
+# - By index positions .iloc[]  - use row numbers to select rows.
+
+
+df.loc[0]  # returns the first row as a Series
+df.loc[0:1]  # returns the first two rows as a DataFrame
+df.loc[0, "city"]  # returns the value in the first row and 'City' column
+
+# NOTE : IN .loc[] When we slice,the end index is inclusive, meaning it includes the row at the end index.
+
+
+df.iloc[1]  # returns the second row as a Series    
+df.iloc[0:2]  # returns the first two rows as a DataFrame
+df.iloc[0, 2]  # returns the value in the first row and third column
+
+# NOTE : IN .iloc[] When we slice, the end index is exclusive, meaning it does not include the row at the end index.
