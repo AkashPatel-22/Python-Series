4e997d86e00ab2e8e260271c7bd2712a2df3db70
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-23 10:06:08 +0530

    query() method

--- Day-48/Filtering-&-QueringData.py
+++ Day-48/Filtering-&-QueringData.py
@@ -14,3 +14,15 @@ print(filtered_df)
 
 
 #  we can use  & for AND, | for OR, and ~ for NOT operations to combine multiple conditions.
+
+
+
+# df.query() method:
+# - we can use the query() method to filter data using a string expression. 
+
+# example:
+df.query('A > 2 and B < 8')  # using query() method to filter data
+df.query('A > 2 or B < 7')  # using query() method with OR condition
+
+# NOTE: The query() method is useful when we have complex filtering conditions and want to write them in a more readable way.
+# NOTE: Qeuery returns a copy , not a view .
