85a152482376bc789b778390d30595a5d4d0675b
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-30 20:38:05 +0530

    Combining and Joining

---
+++ Day-54/combining&joining.py
@@ -0,0 +1,11 @@
+# Combining and Joining DataFrames in Pandas
+
+# In this tutorial, we will explore how to combine and join DataFrames in Pandas.
+# We will cover various methods such as concatenation, merging, and joining.
+
+# df.merge() 
+# -is a powerful method that allows you to combine two DataFrames based on a common column or index.
+# It is similar to SQL joins and can be used to perform inner, outer, left, and right joins.
+
+# -Merge is used to combine two DataFrames based on a common column or index.
+# It is similar to SQL joins and can be used to perform inner, outer, left, and right joins.
