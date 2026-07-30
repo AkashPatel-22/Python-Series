fdda1d001e6efa94d1548b25e1b2ef441b8558a6
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-07-30 20:40:16 +0530

    Multiple joins

--- Day-54/combining&joining.py
+++ Day-54/combining&joining.py
@@ -9,3 +9,10 @@
 
 # -Merge is used to combine two DataFrames based on a common column or index.
 # It is similar to SQL joins and can be used to perform inner, outer, left, and right joins.
+
+
+# it supports -
+# Inner Join: Returns only the rows that have matching values in both DataFrames.
+# Outer Join: Returns all rows from both DataFrames, with NaN in places where there are no matches.
+# Left Join: Returns all rows from the left DataFrame and the matched rows from the right DataFrame. If there is no match, NaN is returned for the right DataFrame.
+# Right Join: Returns all rows from the right DataFrame and the matched rows from the left DataFrame. If there is no match, NaN is returned for the left DataFrame. 
